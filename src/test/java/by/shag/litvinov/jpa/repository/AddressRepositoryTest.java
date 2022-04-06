542420cb790421c9c726e44a482ea43b594fa5bf
Dmitry_Litvinov <6301018@rambler.ru>
2022-04-06 18:56:16 +0300

    Start tests: empty AddressRepositoryTest

---
+++ src/test/java/by/shag/litvinov/jpa/repository/AddressRepositoryTest.java
@@ -0,0 +1,12 @@
+package by.shag.litvinov.jpa.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressRepositoryTest {
+
+    @Test
+    void findByCountryAndCityAndStreetAndHouseNumber() {
+    }
+}
